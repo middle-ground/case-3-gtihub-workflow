a2ac945add8d455ef28e2bb5e523cbab7b831010
Ayu Sudi <ayusudi.abc@gmail.com>
2022-11-08 04:27:33 +0700

    Clean code and fix feature-a bug

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 console.log("Intiial commit")
 console.log("with Github Workflow");
 console.log("We use pull request and merge by button");
+
+console.log("Before merge button we must think about create pull request between")
+console.log("Branch <feature> to Branch development")
+console.log("Are not allowed to merge to MAIN")
+
+console.log("To prevent this from happening, We can change the default branch to development")
